f5379e16fe37507d4de71d5ba2ddcb5303127f73
Maria <mysiol007@gmail.com>
2023-12-05 16:21:00 +0100

    Add class for Decorator

---
+++ src/main/java/pl/put/poznan/transformer/logic/TextDecorator.java
@@ -0,0 +1,19 @@
+package pl.put.poznan.transformer.logic;
+/**
+ * TextDecorator
+ *
+ */
+/
+
+public abstract class TextDecorator implements TextTransform {
+    protected final TextTransform textTransform;
+
+    public TextDecorator(TextTransform textTransform) {
+        this.textTransform = textTransform;
+    }
+    //metoda do przekazania tekstu do dekorowanego obiektu
+    @Override
+    public String apply(String text) {
+        return textTransform.apply(text);
+    }
+}
