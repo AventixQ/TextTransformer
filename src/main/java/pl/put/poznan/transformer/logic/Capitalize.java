86c204d8733f22c6967c87929d4fc46b4b6ce648
Maria <mysiol007@gmail.com>
2023-12-05 19:19:42 +0100

    Adding documentation

--- src/main/java/pl/put/poznan/transformer/logic/Capitalize.java
+++ src/main/java/pl/put/poznan/transformer/logic/Capitalize.java
@@ -1,19 +1,34 @@
 package pl.put.poznan.transformer.logic;
 
+/**
+ * Klasa Capitalize jest dekoratorem, który dodaje funkcjonalność zmiany pierwszej litery tekstu na dużą, a resztę na małe litery.
+ * Dziedziczy ona po klasie TextDecorator i implementuje interfejs TextTransform.
+ */
 public class Capitalize extends TextDecorator {
 
+    /**
+     * Konstruktor klasy Capitalize.
+     *
+     * @param textTransform Obiekt implementujący interfejs TextTransform, który ma zostać udekorowany.
+     */
     public Capitalize(TextTransform textTransform) {
         super(textTransform);
     }
 
+    /**
+     * Zastosowuje transformację do tekstu, zmieniając pierwszą literę na dużą, a resztę na małe litery.
+     *
+     * @param text Tekst, do którego ma zostać zastosowana transformacja.
+     * @return Przekształcony tekst.
+     */
     @Override
     public String apply(String text) {
         String transformedText = super.apply(text);
-        // Sprawdzamy, czy tekst nie jest pusty
+
         if (transformedText.length() > 0) {
-            // Zamieniamy pierwszą literę na dużą, a resztę na małe litery
             transformedText = transformedText.substring(0, 1).toUpperCase() + transformedText.substring(1).toLowerCase();
         }
+
         return transformedText;
     }
 }
--- src/main/java/pl/put/poznan/transformer/logic/Uppercase.java
+++ src/main/java/pl/put/poznan/transformer/logic/Uppercase.java
@@ -1,14 +1,28 @@
 package pl.put.poznan.transformer.logic;
 
-public class Uppercase extends TextDecorator{
+/**
+ * Klasa Uppercase jest dekoratorem, który dodaje funkcjonalność zamiany wszystkich liter tekstu na duże litery.
+ * Dziedziczy ona po klasie TextDecorator i implementuje interfejs TextTransform.
+ */
+public class Uppercase extends TextDecorator {
 
+    /**
+     * Konstruktor klasy Uppercase.
+     *
+     * @param textTransform Obiekt implementujący interfejs TextTransform, który ma zostać udekorowany.
+     */
     public Uppercase(TextTransform textTransform) {
         super(textTransform);
     }
 
+    /**
+     * Zastosowuje transformację do tekstu, zamieniając wszystkie litery na duże.
+     *
+     * @param text Tekst, do którego ma zostać zastosowana transformacja.
+     * @return Przekształcony tekst.
+     */
     @Override
-    public String apply(String text)
-    {
+    public String apply(String text) {
         String transformedText = super.apply(text);
         return transformedText.toUpperCase();
     }
