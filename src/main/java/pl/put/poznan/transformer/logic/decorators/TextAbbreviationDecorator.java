bbdddc267a2ef94bf98fa1780d99e41216e57ae9
Szymon <s.szymankiewicz@icloud.com>
2023-12-06 14:57:01 +0100

    Add functionality to TextAbbreviationDecorator

--- src/main/java/pl/put/poznan/transformer/logic/decorators/TextAbbreviationDecorator.java
+++ src/main/java/pl/put/poznan/transformer/logic/decorators/TextAbbreviationDecorator.java
@@ -3,12 +3,24 @@ package pl.put.poznan.transformer.logic.decorators;
 import pl.put.poznan.transformer.logic.TextDecorator;
 import pl.put.poznan.transformer.logic.TextTransform;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * This is class for creating abbreviations in text.
  */
 
 public class TextAbbreviationDecorator extends TextDecorator {
     public TextAbbreviationDecorator(TextTransform textTransform) { super(textTransform); }
+
+    private static final Map<String, String> ABBREVIATIONS_MAP = new HashMap<>();
+
+    //zbiór reguł słów i ich skrótów
+    static {
+        ABBREVIATIONS_MAP.put("na przykład", "np.");
+        ABBREVIATIONS_MAP.put("między innymi", "m.in.");
+        ABBREVIATIONS_MAP.put("i tym podobne", "itp.");
+    }
 
     /**
      * @param text - text obtained from client
@@ -17,8 +29,14 @@ public class TextAbbreviationDecorator extends TextDecorator {
 
     @Override
     public String apply(String text) {
-        //TODO zrobic skróty
+        String lowerCaseText = text.toLowerCase();
 
-        return super.apply(text);
+        for (Map.Entry<String, String> entry : ABBREVIATIONS_MAP.entrySet()) {
+            String wordToReplace = entry.getKey().toLowerCase();
+            String abbreviation = entry.getValue();
+            lowerCaseText = lowerCaseText.replaceAll("\\b" + wordToReplace + "\\b", abbreviation);
+        }
+
+        return super.apply(lowerCaseText);
     }
 }
