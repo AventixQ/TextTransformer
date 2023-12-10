bcebb1ca943f6a58f01e8da717f2dcf9726e5c62
Szaptor <bartosz.zawadzki.g4@gmail.com>
2023-12-10 23:46:34 +0100

    added logger and jar in pom, added logger in textinversedecorator

--- src/main/java/pl/put/poznan/transformer/logic/decorators/TextInverseDecorator.java
+++ src/main/java/pl/put/poznan/transformer/logic/decorators/TextInverseDecorator.java
@@ -3,10 +3,14 @@ package pl.put.poznan.transformer.logic.decorators;
 import pl.put.poznan.transformer.logic.TextDecorator;
 import pl.put.poznan.transformer.logic.TextTransform;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 /**
  * This is class for inverting the text.
  */
 public class TextInverseDecorator extends TextDecorator {
+
+    private static Logger logger = LoggerFactory.getLogger(TextInverseDecorator.class);
     public TextInverseDecorator(TextTransform textTransform) {
         super(textTransform);
     }
@@ -17,6 +21,11 @@ public class TextInverseDecorator extends TextDecorator {
      */
     @Override
     public String apply(String text) {
-        return super.apply((new StringBuilder(text)).reverse().toString()); //Creating new StringBuilder to use the method reverse on it :D
+        var reversed = new StringBuilder(text).reverse().toString();
+        logger.debug(String.format("text: %s, reversed: %s",text,reversed));
+        logger.info("Successfully reversed!");
+        return super.apply(reversed); //Creating new StringBuilder to use the method reverse on it
     }
+
+
 }
